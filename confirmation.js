8ea6c8ac7db5765fcb166e669ce78fe7069c1bbc
Steven Ranarivelo <63259236+RanaSteven@users.noreply.github.com>
2021-01-29 23:05:11 +0100

    Delete confirmation.js

--- confirmation.js
+++
@@ -1,9 +0,0 @@
-var numeroIdentifiant = localStorage.getItem("numIdentifiant");
-var numId = document.getElementById("numId");
-var total = localStorage.getItem("finalPrice");
-var finalPrice = document.getElementById("finalPrice");
-
-finalPrice.innerHTML = "<span class='font-weight-bold h5'>"+new Intl.NumberFormat("fr-FR", {style: "currency", currency : "EUR"}).format(total)+"</span>";
-numId.innerHTML = "<span class='font-weight-bold h5'>"+numeroIdentifiant+"</span>";
-
-/*localStorage.clear();*/
